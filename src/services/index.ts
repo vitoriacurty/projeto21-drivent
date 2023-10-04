cb07a9c3b27f47732cfa88708194f68a032c1729
vitoriacurty <vitoria.curty@hotmail.com>
2023-10-04 09:14:29 -0300

    template projeto

---
+++ src/services/index.ts
@@ -0,0 +1,7 @@
+export * from './users-service';
+export * from './authentication-service';
+export * from './events-service';
+export * from './enrollments-service';
+export * from './tickets-service';
+export * from './payments-service';
+export * from './hotels-service';
